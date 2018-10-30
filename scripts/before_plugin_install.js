856ef11179971fd4d86c999d81b2b632f4763e3b
skwas <skwas@skwas.net>
2018-10-31 00:20:11 +0100

    Add hook to modify the AppInfo.java file with the namespace of the package. This allows us to extend BuildConfig with extra property via Gradle.

---
+++ scripts/before_plugin_install.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+/**
+ * Android specific before plugin install hook, that modifies our AppInfo.java for the Cordova Android platform.
+ */
+ 
+module.exports = function (context) {
+  var path = require("path");
+  var fs = require("fs");
+  var DOMParser = require("xmldom").DOMParser;
+  var domParser = new DOMParser();
+
+  // Read app id from config.xml.
+  var configXmlFile = path.join(context.opts.projectRoot, "config.xml");
+  var configXml = fs.readFileSync(configXmlFile, "utf-8");
+  var doc = domParser.parseFromString(configXml, "text/xml");
+  var app_id = doc.documentElement.getAttribute("id"); 
+
+  // Modify the AppInfo.java file.
+  var javaFile = path.join(context.opts.plugin.dir, "src/android/AppInfo.java");
+  var fileContents = fs.readFileSync(javaFile, "utf-8");
+  var newFileContents = fileContents.replace(/\[app_id\]/, app_id);
+  fs.writeFileSync(javaFile, newFileContents, "utf-8");
+}
--- src/android/AppInfo.java
+++ src/android/AppInfo.java
@@ -1,36 +1,8 @@
-/*
-The MIT License (MIT)
-
-Copyright (c) 2015 skwas
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
-
 package com.skwas.cordova.appinfo;
 
-import java.io.IOException;
-import java.lang.reflect.Method;
-import java.text.SimpleDateFormat;
-import java.util.Locale;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
-
+import android.app.Activity;
+import android.content.pm.ApplicationInfo;
+import android.os.Build;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
@@ -39,12 +11,11 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.os.Build;
-import android.util.Log;
+import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import [app_id].BuildConfig;
 
 public class AppInfo extends CordovaPlugin {
 
@@ -59,22 +30,22 @@ public class AppInfo extends CordovaPlugin {
 	}
 
 	@Override
-	public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+	public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) {
 		if (action.equals("getAppInfo")) {
 			cordova.getThreadPool().execute(new Runnable() {
 				@Override
 				public void run() {
 					JSONObject result = new JSONObject();
 					try {
-						result.put("name", getApplicationName());
-						result.put("version", getVersion());
-						result.put("build", getBuild());
-						result.put("identifier", getIdentifier());
+						result.put("name", _activity.getString(_appInfo.labelRes));
+						result.put("version", BuildConfig.VERSION_NAME);
+						result.put("build", BuildConfig.VERSION_CODE);
+						result.put("identifier", BuildConfig.APPLICATION_ID);
 						result.put("compileDate", getBuildDate());
+						result.put("compileDateTicks", BuildConfig.BUILD_TIME);
 						result.put("isHardwareAccelerated", getIsHardwareAccelerated());
-						result.put("isDebuggable", getIsDebuggable());
+						result.put("isDebuggable", BuildConfig.DEBUG);
 					} catch(JSONException e) {}
-					Log.i("AppInfo", getBuildDate());
 					callbackContext.success(result);
 				}
 			});
@@ -83,24 +54,11 @@ public class AppInfo extends CordovaPlugin {
 		return false;
 	}
 
-	public String getApplicationName() {
-		int stringId = _appInfo.labelRes;
-		return _activity.getString(stringId);
-	}
-
-	public String getVersion() {
-		try {            
-			return getPackageInfo().versionName;
-		} catch (NameNotFoundException e) {}
-
-		return "";
-	}
-
 	public String getBuildDate() {
 		String buildDate = new SimpleDateFormat(
 			"yyyy-MM-dd HH:mm:ss.SSS",
 			Locale.getDefault()
-			).format(getBuildDate(_activity)) + "Z";
+			).format(BuildConfig.BUILD_TIME) + "Z";
 		return buildDate.replace(" ", "T");
 	}
 
@@ -113,50 +71,5 @@ public class AppInfo extends CordovaPlugin {
 			} catch (Exception e) {}
 		}
 		return isHardwareAccelerated;
-	}
-
-	public boolean getIsDebuggable() {
-		return 0 != ( _appInfo.flags &= ApplicationInfo.FLAG_DEBUGGABLE );
-	}
-
-	public static long getBuildDate(Context context) {
-		ZipFile zf = null;
-		try {
-			ApplicationInfo ai = context
-				.getPackageManager()
-				.getApplicationInfo(context.getPackageName(), 0);
-			zf = new ZipFile(ai.sourceDir);
-			ZipEntry ze = zf.getEntry("classes.dex");
-			long time = ze.getTime();
-			return time;
-
-		} catch (Exception e) {
-		}
-
-		finally {
-			try {
-				if (zf != null) zf.close();
-			} catch (IOException e) {}
-		}
-
-		return 0l;
-	}
-
-	public String getBuild() {
-		try {
-			return Integer.toString(getPackageInfo().versionCode);
-		} catch (NameNotFoundException e) {
-			return null;
-		}
-	}
-
-	public String getIdentifier() {
-		return _activity.getPackageName();
-	}
-
-	private android.content.pm.PackageInfo getPackageInfo() throws NameNotFoundException {
-		return _activity
-			.getPackageManager()
-			.getPackageInfo(getIdentifier(), 0);
 	}
 }
